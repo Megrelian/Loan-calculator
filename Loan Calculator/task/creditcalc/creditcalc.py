cf1d72bc0c92f5b3c4e74640707d68110edd756a
Megrelian <ditojalagonia@gmail.com>
2021-02-15 14:41:59 +0400

    update

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,7 +1,18 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
-
-# write your code here
+import math
+print("Enter the loan principal:")
+principal = int(input())
+print("What do you want to calculate?")
+print("type 'm' for number of monthly payments,\ntype 'p' for the monthly payment")
+what_to_calculate = input()
+if what_to_calculate == "m":
+    montly_payment = int(input("Enter the monthly payment:\n"))
+    number_of_month = round(principal / montly_payment)
+    if number_of_month > 1:
+        print(f"It will take {number_of_month} months to repay the loan")
+    else:
+        print(f"It will take {number_of_month} month to repay the loan")
+elif what_to_calculate == "p":
+    number_of_month = int(input("Enter the number of months: \n"))
+    monthly_payment = math.ceil(principal / number_of_month)
+    last_month = principal - (number_of_month -1) * monthly_payment
+    print(f"Your monthly payment = {monthly_payment} and the last payment = {last_month}.")
